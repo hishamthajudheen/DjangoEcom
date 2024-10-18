53532e3c8295a96e9d1b4f125650bd6d45db26db
Hisham Thajudheen <hishamthajudheen@gmail.com>
2024-10-19 02:12:44 +0530

    Update Cart Function refinement 2

--- cart/cart.py
+++ cart/cart.py
@@ -1,4 +1,4 @@
-
+from core.models import Product
 class Cart():
     def __init__(self, request):
         self.session = request.session
@@ -13,14 +13,38 @@ class Cart():
         # Making cart available on all pages        
         self.cart = cart
         
-    def add(self, product):
+    def add(self, product, quantity):
         product_id = str(product.id)
+        product_qty = str(quantity)
         if product_id in self.cart:
             pass
         else:
-            self.cart[product_id] = {'price': str(product.price)}  
+            #self.cart[product_id] = {'price': str(product.price)} 
+            self.cart[product_id] = int(product_qty)
                       
-        self.session.modified = True 
+        self.session.modified = True
+
         
     def __len__(self):
         return len(self.cart)
+    
+    def get_products(self):
+        product_ids = self.cart.keys()
+        products = Product.objects.filter(id__in=product_ids) 
+        return products
+    
+    def get_quants(self):
+        quantities = self.cart
+        return quantities
+    
+    def update(self, product, quantity):
+        product_id = str(product)
+        product_qty = int(quantity)
+        
+        ourcart = self.cart
+        ourcart[product_id] = product_qty
+        
+        self.session.modified = True
+        
+        return self.cart
+    
--- cart/views.py
+++ cart/views.py
@@ -6,15 +6,19 @@ from django.http import JsonResponse
 # Create your views here.
 
 def cart_summary(request):
-    return render(request, "cart_summary.html", {})
+    cart = Cart(request)
+    cart_products = cart.get_products
+    quantities = cart.get_quants
+    return render(request, "cart_summary.html", {"cart_products": cart_products, "quantities": quantities})
 
 
 def cart_add(request):
     cart = Cart(request)
     if request.POST.get('action') == 'post':
         product_id = int(request.POST.get('product_id'))
+        product_qty = int(request.POST.get('product_qty'))
         product = get_object_or_404(Product, id=product_id)
-        cart.add(product=product)
+        cart.add(product=product, quantity=product_qty)
         
         #response = JsonResponse({'Product Name: ': product.name})
         cart_quantity = cart.__len__()
@@ -26,4 +30,11 @@ def cart_delete(request):
 
 
 def cart_update(request):
-    pass
+    cart = Cart(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('product_id'))
+        product_qty = int(request.POST.get('product_qty'))
+        
+        cart.update(product=product_id, quantity = product_qty)
+        response = JsonResponse({'qty':product_qty})
+        return response
