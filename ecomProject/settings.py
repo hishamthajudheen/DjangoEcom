f0736d28b29c9aa3c50c6d5d6ac860a1c9fe2dd5
Hisham Thajudheen <hishamthajudheen@gmail.com>
2024-10-19 02:21:23 +0530

    added render to trusted CSRF origin

--- ecomProject/settings.py
+++ ecomProject/settings.py
@@ -135,3 +135,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['https://djangoecom-mua5.onrender.com']
+
